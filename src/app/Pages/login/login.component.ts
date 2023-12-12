42044964bff1289a2614eda82c19fe61e3cdcc1b
samuelscp11917 <samuelscp11917@gmail.com>
2023-12-12 18:43:32 -0500

    login

---
+++ src/app/Pages/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule ,],
+  templateUrl: './login.component.html',
+  styleUrl:'./login.component.css'
+})
+export class LoginComponent {
+  showRegister = false;
+
+  toggleView() {
+    this.showRegister = !this.showRegister;
+    console.log(this.showRegister)
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginComponent } from './Pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "Log-in", component : LoginComponent},
+];
